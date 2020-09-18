be1753936b2c61088dda65908aa12ece3a1f5b52
preeti rout <preetichhandarout@gmail.com>
2020-09-18 21:57:55 +0530

    add product commit 2 by preeti

--- Puspa Karmakar-170301120138/dashboard/apiCall.js
+++ Puspa Karmakar-170301120138/dashboard/apiCall.js
@@ -14,3 +14,25 @@
             $log.log('Error occured..'+resp.data)
         });
         });
+
+app.controller('ProductController',function ($scope, $http, $log) {
+  
+  $scope.catName;
+  $scope.productName;
+  $scope.description;
+  $scope.price;
+  $scope.shgId;
+  $scope.noOfAvailability;
+
+  $scope.postData = function () {
+
+    $http({
+      url : "http://localhost:8080/product/addProduct" + "/" + $scope.catName+ "/" + $scope.productName + "/" +$scope.description + "/" + $scope.price + "/" + $scope.shgId + "/" + $scope.noOfAvailability,
+      method : "GET"
+    }).then(function(response){
+      $scope.result = response.data;
+    },function(response){
+      alert("Error is there"+ response.data);
+    });
+  };
+});
