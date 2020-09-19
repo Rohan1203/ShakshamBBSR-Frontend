540f44ec65d61d6c8d106b5d2505b7e8076a8889
preeti rout <preetichhandarout@gmail.com>
2020-09-19 12:01:40 +0530

    shg registration update commit by Preeti-098

--- Puspa Karmakar-170301120138/dashboard/apiCall.js
+++ Puspa Karmakar-170301120138/dashboard/apiCall.js
@@ -14,7 +14,7 @@
             $log.log('Error occured..'+resp.data)
         });
         });
-
+/*product addition*/
 app.controller('ProductController',function ($scope, $http, $log) {
   
   $scope.catName;
@@ -28,6 +28,36 @@ app.controller('ProductController',function ($scope, $http, $log) {
 
     $http({
       url : "http://localhost:8080/product/addProduct" + "/" + $scope.catName+ "/" + $scope.productName + "/" +$scope.description + "/" + $scope.price + "/" + $scope.shgId + "/" + $scope.noOfAvailability,
+      method : "GET"
+    }).then(function(response){
+      $scope.result = response.data;
+    },function(response){
+      alert("Error is there"+ response.data);
+    });
+  };
+});
+/*shg addition */
+app.controller('shgController',function ($scope, $http, $log) {
+  
+  $scope.shgId;
+  $scope.shgName;
+  $scope.address;
+  $scope.blockName;
+  $scope.district;
+  $scope.state;
+  $scope.pincode;
+  $scope.mobileNumber;
+  $scope.noOfMember;
+  $scope.aadharNumber;
+  $scope.accountNumber;
+  $scope.ifscCode;
+  $scope.shgEmail;
+  
+
+  $scope.postData = function () {
+
+    $http({
+      url : "http://localhost:8080/product/addProduct" + "/" + $scope.shgId+ "/" + $scope.shgName + "/" +$scope.addresss + "/" + $scope.blockName + "/" + $scope.district + "/" + $scope.state + "/" + $scope.pincode + "/" + $scope.mobileNumber + "/" +$scope.noOfMember + "/" + $scope.aadharNumber + "/" + $scope.accountNumber + "/" + $scope.ifscCode + "/" + $scope.shgEmail,
       method : "GET"
     }).then(function(response){
       $scope.result = response.data;
