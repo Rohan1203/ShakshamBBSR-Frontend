d180299b32394163daac3991d9278fa6cb26b939
star <rohan.das1203@gmail.com>
2020-09-24 20:33:48 +0530

    Login to Dashboard Done

--- js/app.js
+++ js/app.js
@@ -1,71 +1,51 @@
 'use strict'
 
-var app = angular.module('shg-app', []);
+var app = angular.module('shg-app', ['ngRoute']);
 
 //Rohan's code start
-app.config([ '$routeProvider', '$locationProvider',
-function($routeProvider, $locationProvider) {
-      $routeProvider.when('/dashboard', {
-          templateUrl : "../Puspa_Karmakar-170301120138/dashboard/adminDashboard.html",
-          controller : ''
-      })
-      // $routeProvider.when('/', {
-      //     templateUrl : 'index.html',
-      //     controller : 'indexController'
-      // }).otherwise({
-      //     redirectTo : 'adminDashboard.html'
-      // });
-      //$locationProvider.html5Mode(true); //Remove the '#' from URL.
-  }
-]);
+
 
 app.controller('indexController', function ($scope, $http, $location) {
-	$scope.username;
-	$scope.password;
 
-	$scope.postData = function () {
 
-		$http.post("http://localhost:8080/user?username=" + $scope.username + "&password=" + $scope.password)
-		.then(function(resp) {
-			$scope.token = resp.data.token;
-			$scope.username = resp.data.username;
+  $scope.postData = function () {
 
-			if ($scope.token == null) {
-				 alert("Please provide correct username and password");
-			}
-			else{
-        $scope.result = resp.data.token;
-				 $location.path("../Puspa_Karmakar-170301120138/dashboard/adminDashboard.html" );
-			}
-		}, function(resp){
-				 alert('Error occured during fetching data..' + resp.data);
-		});
-	 };
+    $http.post("http://localhost:8080/user?username=" + $scope.username + "&password=" + $scope.password)
+      .then(function (resp) {
+        $scope.token = resp.data.token;
+        $scope.username = resp.data.username;
+
+        if ($scope.token == null) {
+          alert("Server not responding");
+        }
+        else {
+          location.replace("../dashboard/adminDashboard.html");
+        }
+      }, function (resp) {
+        alert('Error occured during fetching data..' + resp.data);
+      });
+  };
 });
 //Rohan's code end
 
+//puspa's code
+app.controller('all-shg', function ($scope, $http, $log) {
+  $http({
+    url:
+      'http://localhost:8080/shgrequests',
+    method: 'GET'
+  }).then(function (resp) {
+    $log.log(resp.data);
+    $scope.data = resp.data;
+  }, function (resp) {
 
-
-
-        
-        
-        app.controller('all-shg', function($scope,$http,$log){
-            $http({
-            url:
-            'http://localhost:8080/shgrequests',
-            method: 'GET'
-            }).then(function(resp){
-            $log.log(resp.data);
-            $scope.data = resp.data;
-        }, function(resp){
-            
-            $log.log('Error occured..'+resp.data)
-        });
-        });
+    $log.log('Error occured..' + resp.data)
+  });
+});
 
 /*product addition*/
-app.controller('ProductController',function ($scope, $http, $log) {
-  
+app.controller('ProductController', function ($scope, $http, $log) {
+
   $scope.catName;
   $scope.productName;
   $scope.description;
@@ -76,19 +56,19 @@ app.controller('ProductController',function ($scope, $http, $log) {
   $scope.postData = function () {
 
     $http({
-      url : "http://localhost:8080/product/addProduct" + "/" + $scope.catName+ "/" + $scope.productName + "/" +$scope.description + "/" + $scope.price + "/" + $scope.shgId + "/" + $scope.noOfAvailability,
-      method : "GET"
-    }).then(function(response){
+      url: "http://localhost:8080/product/addProduct" + "/" + $scope.catName + "/" + $scope.productName + "/" + $scope.description + "/" + $scope.price + "/" + $scope.shgId + "/" + $scope.noOfAvailability,
+      method: "GET"
+    }).then(function (response) {
       $scope.result = response.data;
-    },function(response){
-      alert("Error is there"+ response.data);
+    }, function (response) {
+      alert("Error is there" + response.data);
     });
   };
 });
 
 /*shg addition */
-app.controller('shgController',function ($scope, $http, $log) {
-  
+app.controller('shgController', function ($scope, $http, $log) {
+
   $scope.shgId;
   $scope.shgName;
   $scope.address;
@@ -102,25 +82,25 @@ app.controller('shgController',function ($scope, $http, $log) {
   $scope.accountNumber;
   $scope.ifscCode;
   $scope.shgEmail;
-  
+
 
   $scope.postData = function () {
 
     $http({
-      url : "http://localhost:8080/product/addProduct" + "/" + $scope.shgId+ "/" + $scope.shgName + "/" +$scope.addresss + "/" + $scope.blockName + "/" + $scope.district + "/" + $scope.state + "/" + $scope.pincode + "/" + $scope.mobileNumber + "/" +$scope.noOfMember + "/" + $scope.aadharNumber + "/" + $scope.accountNumber + "/" + $scope.ifscCode + "/" + $scope.shgEmail,
-      method : "GET"
-    }).then(function(response){
+      url: "http://localhost:8080/product/addProduct" + "/" + $scope.shgId + "/" + $scope.shgName + "/" + $scope.addresss + "/" + $scope.blockName + "/" + $scope.district + "/" + $scope.state + "/" + $scope.pincode + "/" + $scope.mobileNumber + "/" + $scope.noOfMember + "/" + $scope.aadharNumber + "/" + $scope.accountNumber + "/" + $scope.ifscCode + "/" + $scope.shgEmail,
+      method: "GET"
+    }).then(function (response) {
       $scope.result = response.data;
-    },function(response){
-      alert("Error is there"+ response.data);
+    }, function (response) {
+      alert("Error is there" + response.data);
     });
   };
 });
 
 
 /* category addition */
-app.controller('categoryController',function ($scope, $http, $log) {
- 
+app.controller('categoryController', function ($scope, $http, $log) {
+
   $scope.catName;
   $scope.catType;
 
@@ -128,12 +108,12 @@ app.controller('categoryController',function ($scope, $http, $log) {
   $scope.postData = function () {
 
     $http({
-      url : "http://localhost:8080/category/addCategory" +  "/" +  $scope.catName + "/" + $scope.catType,
-      method : "GET"
-    }).then(function(response){
+      url: "http://localhost:8080/category/addCategory" + "/" + $scope.catName + "/" + $scope.catType,
+      method: "GET"
+    }).then(function (response) {
       $scope.result = response.data;
-    },function(response){
-      alert("Error is there"+ response.data);
+    }, function (response) {
+      alert("Error is there" + response.data);
     });
   };
 });
